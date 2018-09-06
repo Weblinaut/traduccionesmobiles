ef45b14260e7a6acc4860c5a85b4afbaedd66ac7
Weblinaut <skydiver76@gmail.com>
2018-09-06 16:52:09 -0400

    having fun

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Translate} from "react-localize-redux";
 
 
 class Footer extends Component {
@@ -6,7 +7,7 @@ class Footer extends Component {
         return (
             <footer className="footer-section text-center">
                 <div className="container">
-                    <h2 className="section-title mb-5">Let’s work together</h2>
+                    <h2 className="section-title mb-5"><Translate id='services.workTogether'/></h2>
                     <a href="" className="site-btn">Get in touch</a>
                     <div className="social-links">
                         <a href=""><span className="fa fa-pinterest"></span></a>
